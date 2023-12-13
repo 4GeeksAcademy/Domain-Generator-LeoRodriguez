71d50317f3218f194f8a2b3ccc22f12fec1a0fbb
Leonardo Rodriguez <106454639+Leonard392@users.noreply.github.com>
2023-12-13 18:03:25 +0000

    complete

--- src/app.js
+++ src/app.js
@@ -2,10 +2,45 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#generator").innerHTML = generateDomain();
 };
+
+const shuffleArray = Array => {
+  const shuffledArray = Array.slice();
+
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+
+  return shuffledArray;
+};
+
+const generateDomain = () => {
+  let pronoun = shuffleArray(["the", "our"]);
+  let adj = shuffleArray(["great", "big"]);
+  let noun = shuffleArray(["jogger", "racoon"]);
+
+  let containerSelect = document.querySelector(".Domains");
+  containerSelect.innerHTML = "";
+  for (let i = 0; i < 2; i++) {
+    for (let j = 0; j < 2; j++) {
+      for (let k = 0; k < 2; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+
+        let newChild = document.createElement("h3");
+        newChild.innerText = pronoun[i] + adj[j] + noun[k] + ".com";
+        containerSelect.append(newChild);
+      }
+    }
+  }
+};
+
+const reloadBtn = document.querySelector(".btn");
+const reload = () => {
+  generateDomain();
+};
+
+reloadBtn.addEventListener("click", reload);
